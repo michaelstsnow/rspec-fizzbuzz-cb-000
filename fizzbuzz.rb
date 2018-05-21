064f12f21626741872f38a0aec9653fe25e4389c
michaelstsnow <michael.st.snow@gmail.com>
2018-05-21 01:03:38 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,15 @@ def fizzbuzz (input)
   #get the remainders
   div_3_rem=input % 3
   div_5_rem=input % 5
-
+  #then lets test the remainders
+  if div_3_rem==0 && div_5_rem==0
+    return "FizzBuzz"
+  elsif div_3_rem==0
+    return "Fizz"
+  elsif div_5_rem==0
+    return "Buzz"
+  else
+    return nil
+  end
 
 end
