5ab475474834eec876e1e2cb9879ffff2585481a
michaelstsnow <michael.st.snow@gmail.com>
2018-05-21 01:01:57 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,5 @@
-#Lets make the fizzbuss megthod
-def fizzbuzz(input)
-  #First get the remainder
-  div_3_rem=input % 3;
-  div_5_rem=input % 5;
-
-    if div_3_rem==0 && div_5_rem==0
-      return "FizzBuzz"
-    elseif div_3_rem==0
-      return "Fizz"
-    elseif div_5_rem==0
-      return "Buzz"
-    elseif !(div_3_rem==0) && !(div_5_rem==0)
-      return nil
-    end
+#First define the methods
+def fizzbuzz (input)
+  div_3_rem=input % 3
+  div_5_rem=input % 5
 end
